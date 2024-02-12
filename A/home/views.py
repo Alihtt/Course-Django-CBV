1b8edc4a7eb17f37bf8e0c244d25967d45a4fcad
Alihtt <Aliht.workspace@gmail.com>
2024-02-12 21:47:05 +0330

    ep7-CreateView

--- A/home/views.py
+++ A/home/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView
-from django.views.generic import FormView
+from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import CarCreateForm
 from .models import Car
@@ -12,15 +12,28 @@ class HomeView(ListView):
     model = Car
 
 
-class CarCreateView(FormView):
+# class CarCreateView(FormView):
+#     template_name = 'home/create.html'
+#     form_class = CarCreateForm
+#     success_url = reverse_lazy('home:home')
+#
+#     def form_valid(self, form):
+#         self._create_car(form.cleaned_data)
+#         messages.success(self.request, 'Your car created successfully', 'success')
+#         return super().form_valid(form)
+#
+#     def _create_car(self, data):
+#         Car.objects.create(name=data['name'], owner=data['owner'], year=data['year'])
+
+class CarCreateView(CreateView):
     template_name = 'home/create.html'
-    form_class = CarCreateForm
+    model = Car
     success_url = reverse_lazy('home:home')
+    fields = ('name', 'year')
 
     def form_valid(self, form):
-        self._create_car(form.cleaned_data)
+        car = form.save(commit=False)
+        car.owner = self.request.user.username if self.request.user.username else 'nothing'
+        car.save()
         messages.success(self.request, 'Your car created successfully', 'success')
         return super().form_valid(form)
-
-    def _create_car(self, data):
-        Car.objects.create(name=data['name'], owner=data['owner'], year=data['year'])
