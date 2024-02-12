8836a93321f67541864b207579671398c0dfe7f6
Alihtt <Aliht.workspace@gmail.com>
2024-02-12 17:19:25 +0330

    ep2-TemplateView

--- A/home/views.py
+++ A/home/views.py
@@ -1,20 +1,11 @@
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import TemplateView
+from .models import Car
 
 
-# Create your views here.
-class HomeView(View):
-    http_method_names = ['get', 'options']
+class HomeView(TemplateView):
+    template_name = 'home/home.html'
 
-    def get(self, request):
-        return render(request, 'home/home.html')
-
-    def options(self, request, *args, **kwargs):
-        response = super().options(request, *args, **kwargs)
-        response.headers['user'] = request.user
-        response.headers['host'] = 'localhost'
-        return response
-
-    def http_method_not_allowed(self, request, *args, **kwargs):
-        super().http_method_not_allowed(request, *args, **kwargs)
-        return render(request, 'method_not_allowed.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cars'] = Car.objects.all()
+        return context
